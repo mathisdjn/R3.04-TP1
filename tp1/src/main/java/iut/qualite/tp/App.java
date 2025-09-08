71ea8811066f6630ef8b72b50465531bd98351b0
Sorions <sorions971@gmail.com>
2025-09-08 10:19:08 +0200

    added maven structure

---
+++ tp1/src/main/java/iut/qualite/tp/App.java
@@ -0,0 +1,13 @@
+package iut.qualite.tp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
